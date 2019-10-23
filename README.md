a2a937418381c0f88da7f6d4b4e5b992ca0a746c
bobbyjoe91 <bobby.cool00763@gmail.com>
2019-10-24 05:40:06 +0700

    template for login, register, main menu

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Application for Managing Finance and Inventory. Dedicated to Offline Store / Merchant
 <br>
 The project is created in the virtual_env
+
+<br>
+
+#### Install Mysql Client
+
+pip install mysqlclient
+
--- virtual_env/FINAMA/FINAMA/urls.py
+++ virtual_env/FINAMA/FINAMA/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Desk import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', views.index),
+    path('register/', views.register),
+    path('login/', views.login)
 ]
