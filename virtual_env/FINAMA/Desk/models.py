dd000c9636c77aa9bcfbe0791589f09c7d6cf561
bobbyjoe91 <bobby.cool00763@gmail.com>
2019-10-25 14:10:19 +0700

    Models created (some still empty) WARNING: Unmigrated

--- virtual_env/FINAMA/Desk/models.py
+++ virtual_env/FINAMA/Desk/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Pengeluaran(models.Model):
+    id_pengeluaran = models.IntegerField(primary_key=True)
+    deskripsi_pengeluaran = models.CharField(max_length=150, null=True)
+    biaya = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.id_pengeluaran
+
+class Piutang(models.Model):
+    pass
+
+class Barang(models.Model):
+    pass
+
+# These models below have foreign key(s)
+class Pendapatan(models.Model):
+    pass
+
+class Profit(models.Model):
+    pass
